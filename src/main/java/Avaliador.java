6cf7223189943666108c50e1d8e9e6a2b23bc1c3
felipe <felipe.peixoto@msn.com>
2020-05-29 23:13:05 -0300

    Emplementando a classe recomendaLivros

---
+++ src/main/java/Avaliador.java
@@ -0,0 +1,5 @@
+import org.apache.mahout.cf.taste.common.TasteException;
+import org.apache.mahout.cf.taste.eval.RecommenderBuilder;
+
+public class Avaliador {
+}
--- src/main/java/recomendaLivros.java
+++ src/main/java/recomendaLivros.java
@@ -1,2 +1,19 @@
+import org.apache.mahout.cf.taste.common.TasteException;
+import org.apache.mahout.cf.taste.model.DataModel;
+import org.apache.mahout.cf.taste.recommender.RecommendedItem;
+import org.apache.mahout.cf.taste.recommender.Recommender;
+import java.io.IOException;
+import java.util.List;
+
 public class recomendaLivros {
+    public static void main(String[] args)throws TasteException,IOException {
+        DataModel produtos = new recomendador().getModeloLivros();
+        Recommender recommender = new recomendadorBuilder().buildRecommender(produtos);
+        System.out.println("Usuario1");
+        List <RecommendedItem> recomendations = recommender.recommend(1,4);
+        for (RecommendedItem recomendation : recomendations){
+            System.out.println("Usuario 1, estes livros cobinam com você");
+            System.out.println(recomendation);
+        }
+    }
 }

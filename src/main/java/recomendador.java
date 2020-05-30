b2c1f0bedf61f6ea72366c9e9b64a26c825e25bb
felipe <felipe.peixoto@msn.com>
2020-05-30 00:06:01 -0300

    Concluido

--- src/main/java/recomendador.java
+++ src/main/java/recomendador.java
@@ -3,20 +3,21 @@ import org.apache.mahout.cf.taste.model.DataModel;
 
 import java.io.File;
 import java.io.IOException;
-//fazendo a leitura do csv com base de dados livros
+
 public class recomendador {
-    public DataModel getModeloLivros() throws  IOException{
+
+    public DataModel getModeloLivros() throws IOException {
+
         return getModelo("livros.csv");
     }
 
-    private DataModel getModelo (String path) throws IOException {
-        File file = new File("src/main/resources" + path);
+    private DataModel getModelo(String path) throws IOException {
+        File file = new File("src/main/resources/" + path);
         return new FileDataModel(file);
     }
-    //fazendo a leitura do csv com base de dados filmes
-    public DataModel getModeloDeFilmes() throws IOException{
-        return  getModelo("filmes.csv");
+
+    public DataModel getModeloDeFilmes() throws IOException {
+
+        return getModelo("filmes.csv");
     }
-
-
 }
